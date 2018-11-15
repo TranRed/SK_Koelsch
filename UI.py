f4456a4396bbbd26ef68fccd2491450584cf8b2a
TranRed <kupfer.nils@gmail.com>
2018-11-15 20:30:14 +0100

    created main.py
    new file to start the application

--- UI.py
+++ UI.py
@@ -295,13 +295,3 @@ class Ui_MainWindow(object):
         self.pushButton_calculation.setText(_translate("MainWindow", "Kalkulieren"))
         self.menuSoKa.setTitle(_translate("MainWindow", "SoKa"))
 
-
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
-
