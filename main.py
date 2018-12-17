4f9985570e8d08885ef3cd0d9175b3a310e7ba6e
mhabljak <mhabljak@mhabljak-730U3E-740U3E>
2018-12-17 23:52:44 +0100

    some minor cosmetics

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 import view
 import controller
 import model
-from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolbar
-from matplotlib.figure import Figure
 
 class MainWindow(QtWidgets.QMainWindow, view.Ui_MainWindow):
     def __init__(self, parent=None):
