318ea427c35fb927366e6b70035e0f74bdf773f9
TranRed <kupfer.nils@gmail.com>
2019-01-12 16:21:49 +0100

    improved sfg maintenace
    Close #23
    
    - keys and volume are not changable anymor
    - volume is automatically calculated
    - numbers are converted to whole numbers before output
    - check if input is given in whole numbers
    - fixed a bug caused by not handling closeEvent like cancelling

--- popups/sfgControls.py
+++ popups/sfgControls.py
@@ -1,6 +1,17 @@
 from PyQt5 import QtCore, QtWidgets
 import model, utils
 from popups import sfg
+from decimal import *
+import re
+
+
+class customDialog(QtWidgets.QDialog):
+    def __init__(self, parent=None):
+        super().__init__(  )
+
+    #self definde closeEvent needed to have the same handling as cancel button
+    def closeEvent(self,event):
+        exit(self)
 
 def set_first_call(bool):
     global first_call
@@ -11,11 +22,22 @@ def add_sfg(dialog, material):
     dialog.ui.tableWidget.insertRow(rowCount)
     dialog.ui.tableWidget.setItem(rowCount, 0, QtWidgets.QTableWidgetItem(str(rowCount+1)))
     dialog.ui.tableWidget.setItem(rowCount, 1, QtWidgets.QTableWidgetItem(material))
+    dialog.ui.tableWidget.setItem(rowCount, 2, QtWidgets.QTableWidgetItem("0"))
+    dialog.ui.tableWidget.setItem(rowCount, 3, QtWidgets.QTableWidgetItem("0"))
+    dialog.ui.tableWidget.setItem(rowCount, 4, QtWidgets.QTableWidgetItem("0"))
+    dialog.ui.tableWidget.setItem(rowCount, 5, QtWidgets.QTableWidgetItem("0"))
+    dialog.ui.tableWidget.setItem(rowCount, 6, QtWidgets.QTableWidgetItem("0"))
+
+    make_item_not_editable(dialog.ui.tableWidget.item(rowCount,0))
+    make_item_not_editable(dialog.ui.tableWidget.item(rowCount,1))
+    make_item_not_editable(dialog.ui.tableWidget.item(rowCount,6))
+
+    register_item_changed(dialog.ui.tableWidget,rowCount)
 
 def update(dialog, mainUi):
     data = utils.build_list_from_table(dialog.ui.tableWidget)
     for dataset in data:
-        for item in dataset:    
+        for item in dataset:
             if item.text() == '':
                 msg = QtWidgets.QMessageBox()
                 msg.setIcon(QtWidgets.QMessageBox.Warning)
@@ -27,6 +49,7 @@ def update(dialog, mainUi):
     dialog.close()
 
 def exit(dialog):
+    set_first_call(True)
     dialog.close()
 
 def connect_buttons(dialog, mainUi, material):
@@ -34,9 +57,37 @@ def connect_buttons(dialog, mainUi, material):
     dialog.ui.toolButton_confirm.clicked.connect(lambda: update(dialog, mainUi))
     dialog.ui.toolButton_cancel.clicked.connect(lambda: exit(dialog))
 
+def make_item_not_editable(item):
+    item.setFlags(item.flags()  & ~QtCore.Qt.ItemIsEditable
+                                & ~QtCore.Qt.ItemIsEnabled)
+
+def handle_item_change(tableWidget, row):
+    global initializing
+    #only check after data was initialized
+    if initializing == True:
+        return
+    for column in range(2,5):
+        if not re.match("^\d+$",tableWidget.item(row,column).text()):
+            utils.show_message_box(QtWidgets.QMessageBox.Warning,"Bitte geben Sie eine ganze Zahlen ein.","Fehler")
+            return
+
+    newVolume = Decimal(tableWidget.item(row,2).text()) * Decimal(tableWidget.item(row,3).text()) * Decimal(tableWidget.item(row,4).text())
+    tableWidget.item(row,6).setText(str(newVolume))
+
+def register_item_changed(tableWidget, row):
+        tableWidget.itemChanged.connect(lambda: handle_item_change(tableWidget,row))
+
+
+
+def format_number(item):
+    item.setText(str(Decimal(item.text()).quantize(Decimal("0"), ROUND_HALF_UP)))
+
 def show(material, mainUi):
     global first_call
-    dialog = QtWidgets.QDialog()
+    global initializing
+    initializing = True
+
+    dialog = customDialog()
     dialog.ui = sfg.Ui_Dialog()
     dialog.ui.setupUi(dialog)
     dialog.setAttribute(QtCore.Qt.WA_DeleteOnClose)
@@ -50,5 +101,20 @@ def show(material, mainUi):
         set_first_call(False)
     else:
         utils.fill_table_from_list(dialog.ui.tableWidget, model.getSfg())
+
+    for row in range(0,dialog.ui.tableWidget.rowCount()):
+        make_item_not_editable(dialog.ui.tableWidget.item(row,0))
+        make_item_not_editable(dialog.ui.tableWidget.item(row,1))
+        make_item_not_editable(dialog.ui.tableWidget.item(row,6))
+
+        register_item_changed(dialog.ui.tableWidget,row)
+
+        format_number(dialog.ui.tableWidget.item(row,2))
+        format_number(dialog.ui.tableWidget.item(row,3))
+        format_number(dialog.ui.tableWidget.item(row,4))
+        format_number(dialog.ui.tableWidget.item(row,5))
+
+
     connect_buttons(dialog, mainUi, material)
+    initializing = False
     dialog.exec_()
