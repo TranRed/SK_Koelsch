ddf9f17b3e874c185943e262768af83a3b061100
TranRed <kupfer.nils@gmail.com>
2019-02-16 23:04:55 +0100

    check entered volume scalings for duplicates
    check for duplicate volume scalings and sort scalings ascending. closes #28

--- popups/volumeScalingControls.py
+++ popups/volumeScalingControls.py
@@ -30,17 +30,38 @@ def add_volumeScaling(ui):
     ui.tableWidget.insertRow(rowCount)
 
 def update(dialogUi, mainUi):
+    uniques = []
+
+    #set original data once in case validation returns an error
     model.setVolumeScaling(utils.build_list_from_table(dialogUi.tableWidget))
     mainUi.lineEdit_volumeScaling.setText(str(dialogUi.tableWidget.rowCount()))
 
     for row in range(0,dialogUi.tableWidget.rowCount()):
         rowOut = int(row) + int(1)
         if ( dialogUi.tableWidget.item(row,0) == None
-            or not re.match("^\d+$",dialogUi.tableWidget.item(row,0).text())):
-            utils.show_message_box(QtWidgets.QMessageBox.Warning,"Bitte geben Sie eine ganze Zahl in Zeile "+str(rowOut)+" ein.","Fehler")
+            or not re.match("^\d+$",dialogUi.tableWidget.item(row,0).text())
+            or dialogUi.tableWidget.item(row,0).text() == '0' ):
+            utils.show_message_box(QtWidgets.QMessageBox.Warning,"Bitte geben Sie eine ganze Zahl (>0) in Zeile "+str(rowOut)+" ein.","Fehler")
             #@TO-DO: closing and reopening the dialog seems a bit sketchy, better solution needed
             show(mainUi)
             return #only one message at a time
+
+        scaling = int(dialogUi.tableWidget.item(row,0).text())
+        if scaling not in uniques:
+            uniques.append(scaling)
+
+
+    #remove extra lines and adjust texts of remaining lines accordingly
+    while len(uniques) < dialogUi.tableWidget.rowCount():
+        dialogUi.tableWidget.removeRow(dialogUi.tableWidget.rowCount() - 1)
+
+    uniques.sort()
+    for row in range(0,len(uniques)):
+        dialogUi.tableWidget.item(row,0).setText(str(uniques[row]))
+
+    #set final result
+    model.setVolumeScaling(utils.build_list_from_table(dialogUi.tableWidget))
+    mainUi.lineEdit_volumeScaling.setText(str(dialogUi.tableWidget.rowCount()))
 
 def revert_data(oldState):
     model.setVolumeScaling(oldState)
