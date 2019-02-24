9db6962f8841c1f689875122ce01a53a466fd476
TranRed <kupfer.nils@gmail.com>
2019-02-24 16:14:06 +0100

    moved calculations of values to own methods

--- popups/calculationControls.py
+++ popups/calculationControls.py
@@ -39,74 +39,133 @@ def fill_mek_details(mainUi, ui, material, volume, mek):
     matPrice = matPrice.quantize(cent, ROUND_HALF_UP)
     changeLineEdit(ui.lineEdit_materialPrice, matPrice)
 
-def fill_screen(mainUi, ui):
-    volume = int(mainUi.lineEdit_semifinishedSideA.text()) * int(mainUi.lineEdit_semifinishedSideB.text()) * int(mainUi.lineEdit_semifinishedSideC.text())
-    material = model.read_material(mainUi.comboBox_material.currentText()[:6])
-    #educational comment: source for Decimal always has to be a string:
-    """This has to be done because a Python float value, like 135.99, is converted from Python’s language (in base 10)
-        to the computer’s hardware representation (in base 2) and some precision is lost in the process.
-        To avoid the Python language conversion, string literals are used."
-        source: http://buildingskills.itmaybeahack.com/book/programming-2.6/html/p13_modules/p13_c03_decimal.html"""
-    volume = Decimal(str(volume)) / Decimal("1000")
+def calc_material_costs(mainUi,ui,material,volume):
     materialCosts = volume * Decimal(str(material['dichte'])) * Decimal(str(material['preis']))
-    fill_mek_details(mainUi, ui, material, volume, materialCosts)
 
     global cent
     materialCosts = materialCosts.quantize(cent, ROUND_HALF_UP)
     changeLineEdit(ui.lineEdit_mek, materialCosts)
 
+    return materialCosts
+
+def calc_material_overheads(ui):
+    global cent
     materialOverheads = Decimal("0.00")
     materialOverheads = materialOverheads.quantize(cent, ROUND_HALF_UP)
     changeLineEdit(ui.lineEdit_mgk, materialOverheads)
+    return materialOverheads
 
+def calc_manufacturingCosts(ui):
+    global cent
     manufacturingCosts = Decimal("0.00")
     manufacturingCosts = manufacturingCosts.quantize(cent, ROUND_HALF_UP)
     changeLineEdit(ui.lineEdit_fek, manufacturingCosts)
+    return manufacturingCosts
 
-    machine = model.read_machine(mainUi.comboBox_machine.currentText()[:1])
+def calc_manufacturingOverheads(mainUi, ui, machine):
+    global cent
     manufacturingOverheads = Decimal(str(machine['mss'])) * Decimal(str(machine['ruest']))
     manufacturingOverheads = manufacturingOverheads.quantize(cent, ROUND_HALF_UP)
     changeLineEdit(ui.lineEdit_fgk, manufacturingOverheads)
-    fill_fgk_details(ui, machine, manufacturingOverheads)
+    return manufacturingOverheads
 
+def calculate_cogm(ui,materialCosts,materialOverheads,manufacturingCosts,manufacturingOverheads):
+    global cent
     global cogm
     cogm = materialCosts + materialOverheads + manufacturingCosts + manufacturingOverheads
     cogm = cogm.quantize(cent, ROUND_HALF_UP)
     changeLineEdit(ui.lineEdit_cogm, cogm)
 
-    calculate_changables(mainUi, ui)
-
-def calculate_changables(mainUi, ui):
+def calculate_salesOverheads(ui):
     global cent
     global cogm
-
     salesOverheads = cogm * Decimal(str(ui.spinBox_vgk.value())) / Decimal("100")
     salesOverheads = salesOverheads.quantize(cent, ROUND_HALF_UP)
     changeLineEdit(ui.lineEdit_vgk, salesOverheads)
+    return salesOverheads
 
+def calculate_cogs(ui, salesOverheads):
+    global cent
+    global cogm
+    global cogs
     cogs = cogm + salesOverheads
     cogs = cogs.quantize(cent, ROUND_HALF_UP)
     changeLineEdit(ui.lineEdit_cogs, cogs)
 
+def calculate_profit(ui):
+    global cent
+    global cogs
+    global profit
     profit = cogs * Decimal(str(ui.spinBox_profit.value())) / Decimal("100")
     profit = profit.quantize(cent, ROUND_HALF_UP)
     changeLineEdit(ui.lineEdit_profit, profit)
 
+def calculate_skonto(ui):
+    global cent
+    global cogs
+    global profit
+    global skonto
     valueAfterProfit = cogs + profit
     skontoDivisor = (Decimal("100") - Decimal(str(ui.spinBox_skonto.value()))) / Decimal("100")
     skonto = valueAfterProfit / skontoDivisor - valueAfterProfit
     skonto = skonto.quantize(cent, ROUND_HALF_UP)
     changeLineEdit(ui.lineEdit_skonto, skonto)
 
-    valueAfterSkonto = valueAfterProfit + skonto
+def calculate_rebates(ui):
+    global cent
+    global cogs
+    global profit
+    global skonto
+    global rebates
+    valueAfterSkonto = cogs + profit + skonto
     rebateDivisor = (Decimal("100") - Decimal(str(ui.spinBox_rebates.value()))) / Decimal("100")
     rebates = valueAfterSkonto / rebateDivisor - valueAfterSkonto
     rebates = rebates.quantize(cent, ROUND_HALF_UP)
     changeLineEdit(ui.lineEdit_rebates, rebates)
 
-    price = valueAfterSkonto + rebates
+def calculate_price(ui):
+    global cent
+    global cogs
+    global profit
+    global skonto
+    global rebates
+
+    price = cogs + profit + skonto + rebates
     price = price.quantize(cent, ROUND_HALF_UP)
     changeLineEdit(ui.lineEdit_price, price)
+
+def fill_screen(mainUi, ui):
+    volume = int(mainUi.lineEdit_semifinishedSideA.text()) * int(mainUi.lineEdit_semifinishedSideB.text()) * int(mainUi.lineEdit_semifinishedSideC.text())
+    #educational comment: source for Decimal always has to be a string:
+    """This has to be done because a Python float value, like 135.99, is converted from Python’s language (in base 10)
+        to the computer’s hardware representation (in base 2) and some precision is lost in the process.
+        To avoid the Python language conversion, string literals are used."
+        source: http://buildingskills.itmaybeahack.com/book/programming-2.6/html/p13_modules/p13_c03_decimal.html"""
+    volume = Decimal(str(volume)) / Decimal("1000")
+
+    material = model.read_material(mainUi.comboBox_material.currentText()[:6])
+
+    materialCosts = calc_material_costs(mainUi, ui, material, volume)
+    fill_mek_details(mainUi, ui, material, volume, materialCosts)
+
+    materialOverheads = calc_material_overheads(ui)
+
+    manufacturingCosts = calc_manufacturingCosts(ui)
+
+    machine = model.read_machine(mainUi.comboBox_machine.currentText()[:1])
+    manufacturingOverheads = calc_manufacturingOverheads(mainUi, ui, machine)
+    fill_fgk_details(ui, machine, manufacturingOverheads)
+
+    calculate_cogm(ui,materialCosts,materialOverheads,manufacturingCosts,manufacturingOverheads)
+    calculate_changables(mainUi, ui)
+
+def calculate_changables(mainUi, ui):
+    salesOverheads = calculate_salesOverheads(ui)
+    calculate_cogs(ui, salesOverheads)
+    calculate_profit(ui)
+    calculate_skonto(ui)
+    calculate_rebates(ui)
+    calculate_price(ui)
 
 def connect_spinboxes(mainUi, ui):
     ui.spinBox_vgk.valueChanged.connect(lambda: calculate_changables(mainUi, ui))
@@ -123,11 +182,24 @@ def connect_buttons(dialog):
 def create_globals():
     locale.setlocale(locale.LC_ALL, '')
 
+    global cent
+    cent = Decimal("0.01")
+
     global cogm
     cogm = Decimal("0.00")
 
-    global cent
-    cent = Decimal("0.01")
+    global cogs
+    cogs = Decimal("0.00")
+
+    global profit
+    profit = Decimal("0.00")
+
+    global skonto
+    skonto= Decimal("0.00")
+
+    global rebates
+    rebates = Decimal("0.00")
+
 
 
 def show(mainUi):
